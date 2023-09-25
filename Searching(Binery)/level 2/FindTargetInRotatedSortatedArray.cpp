9a2dac1453b82616dfc427c2a2abdb8c59ab6bd7
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-09-25 19:11:13 +0530

    Rework in file level 2

--- Searching(Binery)/level 2/FindTargetInRotatedSortatedArray.cpp
+++ Searching(Binery)/level 2/FindTargetInRotatedSortatedArray.cpp
@@ -1,4 +1,4 @@
-// Find target in rotated and sorted array
+// Find pivot point of array
 
 #include <iostream>
 #include <vector>
@@ -16,17 +16,20 @@ int pivot(vector<int> &arr, int size)
         {
             return mid - 1;
         }
+
         else if (arr[mid] > arr[mid - 1])
         {
             return mid;
         }
-        else if (arr[mid] < arr[start])
-        {
-            end = mid - 1;
-        }
+
         else if (arr[mid] > arr[start])
         {
             start = mid + 1;
+        }
+
+        else if (arr[mid] < arr[start])
+        {
+            end = mid - 1;
         }
 
         else if (arr[start] == arr[end])
@@ -38,76 +41,70 @@ int pivot(vector<int> &arr, int size)
     }
 }
 
-int binarySearch(vector<int> &arr, int size, int target, int pivotIndex)
+int searching(vector<int> &arr, int target, int start, int end)
 {
-    int start = 0;
-    int end = size - 1;
-    int mid = start + (end - start) / 2;
-
     while (start <= end)
     {
-        if (target == arr[mid])
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
         {
             return mid;
         }
-        else if (target > arr[mid])
+        else if (arr[mid] < target)
         {
             start = mid + 1;
         }
-        else if (target < arr[mid])
+        else
         {
             end = mid - 1;
         }
-
-        mid = start + (end - start) / 2;
     }
 
     return -1;
 }
 
-int searchInRotatedArray(vector<int> &arr, int size,int target)
+int lastupdate(vector<int> &arr, int target, int size)
 {
-    int start=0;
-    int end =size-1;
+    int pivotNumber = pivot(arr, size);
+    int result = -1;
 
-    int pivotIndex=pivot(arr,size);
-
-    if (pivotIndex == -1)
+    if (arr[pivotNumber] == target)
     {
-        return binarySearch(arr, start, end, target);
+        result = pivotNumber;
+        return result;
+    }
+    else if (arr[0] <= target && arr[pivotNumber - 1] >= target)
+    {
+        result = searching(arr, target, 0, pivotNumber - 1);
+        return result;
+    }
+    else
+    {
+        result = searching(arr, target, pivotNumber + 1, size - 1);
+        return result;
     }
 
-    if (arr[pivotIndex] == target)
-    {
-        return pivotIndex;
-    }
-    if (arr[0] <= target)
-    {
-        return binarySearch(arr, start, pivotIndex - 1, target);
-    }
-    return binarySearch(arr, pivotIndex + 1, end, target);
-
-    
+    return result;
 }
-
 int main()
 {
     vector<int> arr{11, 12, 13, 14, 15, 16, 2, 4, 6, 8, 10};
     int size = arr.size();
-
     int target;
-    cout << "Enter your target : ";
+
+    cout << "Enter your element : ";
     cin >> target;
 
-    int foundIndex = searchInRotatedArray(arr,size, target);
+    int find = lastupdate(arr, target, size);
 
-    if (foundIndex == -1)
+    if (find == -1)
     {
-        cout << "Not found." << endl;
+        cout << "Element is not found .";
     }
     else
     {
-        cout << "Found at index " << foundIndex << "." << endl;
+        cout << "Element is found .";
     }
 
     return 0;
