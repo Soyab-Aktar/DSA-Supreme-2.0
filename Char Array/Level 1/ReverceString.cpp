28b682379b8e7f040f3f9ed09b4ee9251c43e92c
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-04 19:44:54 +0530

    Modified in reverse file in level1 in charr Array

--- Char Array/Level 1/ReverceString.cpp
+++ Char Array/Level 1/ReverceString.cpp
@@ -1,14 +1,39 @@
 #include<iostream>
 using namespace std;
-void reverse(ch[],100)
+int getlength(char ch[],int size)
 {
-
+  int index=0;
+  int lengthch=0;
+  while(ch[index]!='\0')
+    {
+      index++;
+      lengthch++;
+    }
+  return lengthch;
 }
+
+void revercech(char ch[],int length)
+{
+  int left=0;
+  int right=length-1;
+  while(left<=right)
+    {
+      swap(ch[left],ch[right]);
+      left ++;
+      right--;
+    }
+  cout<<"Reverse form is : "<<ch;
+}
+
 int main()
 {
-    char ch[100];
-    cout<<"Enter your input :";
-    cin.getline(ch,100);
+  char ch[100];
+  int size=100;
+  cout<<"Enter Your input : ";
+  cin.getline(ch,100);
 
-    find(ch,100);
+  int length=getlength(ch,size);
+  revercech(ch,length);
+
+  return 0;
 }
