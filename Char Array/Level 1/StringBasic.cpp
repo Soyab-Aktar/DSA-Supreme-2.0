c30e8f7762e9f9b6eb66c6f2b59f16c166b8735a
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-05 20:26:58 +0530

    New file String Basics

---
+++ Char Array/Level 1/StringBasic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    //cin>>name;
+    cout<<"Enter any words : ";
+    getline(cin,name);
+
+    cout<<"Your ouput is : "<<name;
+
+    return 0;
+}
