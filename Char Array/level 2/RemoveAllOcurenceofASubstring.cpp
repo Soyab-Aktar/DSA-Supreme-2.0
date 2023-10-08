939ccf19a483b68742269d5d3aaaf7c7f796ede6
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-08 22:12:12 +0530

    Modify in Remove Substring file

--- Char Array/level 2/RemoveAllOcurenceofASubstring.cpp
+++ Char Array/level 2/RemoveAllOcurenceofASubstring.cpp
@@ -1,2 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+void removech(string name, string part)
+{
+    while (name.find(part) != string ::npos)
+    {
+        name.erase(name.find(part), part.length());
+    }
+
+    cout << "After remove : " << name;
+}
+int main()
+{
+    string name;
+    string part;
+
+    cout << "Enter you text :";
+    getline(cin, name);
+
+    cout << "Enter your part text :";
+    getline(cin, part);
+
+    removech(name, part);
+
+    return 0;
+}
