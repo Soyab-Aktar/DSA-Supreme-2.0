54bf6367d1e30afd93e1eee163f79c0f792d3f62
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-18 20:02:57 +0530

    New file in pointer basics

---
+++ pointer/level1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    // pointer creation
+    int *ptr = &a;
+
+    // address of a
+    cout << &a << endl;   // give address of a
+    cout << *ptr << endl; // give value stored in address of a
+    cout << ptr << endl;  // address of a
+    cout << &ptr << endl; // give own address of ptr
+
+    return 0;
+}
