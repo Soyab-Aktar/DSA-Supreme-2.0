0524c6ca834bf4ba10266d9664d971aa8d71adcc
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-11-07 09:16:26 +0530

    New file in recurtion class 2

---
+++ 7 Recurtion/Class 2/Printarray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void printArray(int arr[], int size, int index)
+// if you type this its also work
+// void printArray(int *arr, int size, int index)
+{
+    if (index >= size)
+    {
+        return;
+    }
+
+    cout << arr[index] << " ";
+    printArray(arr, size, index + 1);
+}
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index = 0;
+    printArray(arr, size, index);
+    return 0;
+}
