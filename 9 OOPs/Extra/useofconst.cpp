ade80d7ff799e55939bef843073ae477102fc581
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-01-12 11:28:29 +0530

    New file in extra

---
+++ 9 OOPs/Extra/useofconst.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int a = 10;
+    cout << a << endl;
+    // a=5;  reason why we cant assign a new value in a variable,because we constant the value of the variable,it cant be changed
+    return 0;
+}
