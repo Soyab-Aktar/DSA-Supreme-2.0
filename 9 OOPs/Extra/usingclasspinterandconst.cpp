59b316f942a45b9a5be02def110c6ed8c9fd1f6d
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-01-12 19:39:34 +0530

    New file

---
+++ 9 OOPs/Extra/usingclasspinterandconst.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class abc
+{
+    int x;
+    int *y;
+
+public:
+    abc()
+    {
+        x = 0;
+        y = new int(0);
+    }
+    int getx() const
+    {
+        return x;
+    }
+    void setx(int _value)
+    {
+        x = _value;
+    }
+    int gety()
+    {
+        return *y;
+    }
+    void sety(int _value)
+    {
+        *y = _value;
+    }
+};
+int main()
+{
+    abc find;
+    cout << find.getx() << endl;
+    cout << find.gety() << endl;
+}
