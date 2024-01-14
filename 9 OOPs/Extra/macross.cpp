704624a68da27514cbffc2bea982457842801917
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-01-14 11:30:46 +0530

    New file

---
+++ 9 OOPs/Extra/macross.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14 // this is macross it's good for making repeated task little easy
+float circlearea(int num)
+{
+    float final = PI * num * num;
+    return final;
+}
+int main()
+{
+    float num;
+    cout << "Enter your radius of circle :";
+    cin >> num;
+    circlearea(num);
+    cout << "Area of circle is : " << circlearea(num);
+    return 0;
+}
