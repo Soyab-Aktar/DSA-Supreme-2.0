b98b86f76fe7f5c6ad349593e156aa0f2b4856ce
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-05-08 08:17:47 +0530

    New file in class 2

---
+++ 10 Linked List/Class 2/deletiondoubly.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+
+    node()
+    {
+        this->next = NULL;
+        this->prev = NULL;
+    }
+    node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void printll(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int getlen(node *head)
+{
+    node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void insert(node *&head, node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        node *temp = new node(data);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        node *temp = new node(data);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+}
+
+void deletion(node *&head, node *&tail, int pos)
+{
+    int Length = getlen(head);
+
+    if (pos == 1) // deletation from head
+    {
+
+        node *temp = head;
+        head = temp->next;
+        head->prev = NULL;
+        delete temp;
+    }
+    else if (pos == Length)
+    {
+        node *preNode = tail->prev;
+        node *temp = tail;
+        preNode->next = NULL;
+        temp->prev = NULL;
+        delete temp;
+        tail = preNode;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    node *tail = NULL;
+
+    insert(head, tail, 10);
+    insert(head, tail, 20);
+    insert(head, tail, 30);
+    insert(head, tail, 40);
+    insert(head, tail, 50);
+
+    printll(head);
+
+    deletion(head, tail, 4);
+    printll(head);
+
+    return 0;
+}
