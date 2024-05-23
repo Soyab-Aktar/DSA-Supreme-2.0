7fa84a69f6d7e3d746b852abd017e127d5a4d41a
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-05-23 20:05:55 +0530

    New file in class 1 stack

---
+++ 11 Stack/Class 1/insertatbottom.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertbottom(stack<int> &st, int element)
+{
+    // Base Case
+    if (st.empty())
+    {
+        st.push(element);
+        return;
+    }
+
+    // Processing
+    int temp = st.top();
+    st.pop();
+
+    // recursion process
+    insertbottom(st, element);
+
+    // Backtracking
+    st.push(temp);
+}
+
+void display(stack<int> st)
+{
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
+
+int main()
+{
+    stack<int> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    
+    cout << "Original stack: ";
+    display(st);
+
+    int element;
+    cout << "Enter the element you want to insert at the bottom: ";
+    cin >> element;
+
+    insertbottom(st, element);
+
+    cout << "Stack after insertion: ";
+    display(st);
+
+    return 0;
+}
