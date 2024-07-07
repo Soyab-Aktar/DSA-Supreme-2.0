f27fd9dcd107c80a3ca4790e6dbc0a97348a0a6b
Soyab-Aktar <iamsoyabcode23@gmail.com>
2024-07-07 12:44:38 +0530

    New file

---
+++ 13 Binery Tree/Class 3/bottomview.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <queue>
+#include <map>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *tree()
+{
+    int data;
+    cout << "Enter your data : ";
+    cin >> data;
+    if (data == -1)
+    {
+        return NULL;
+    }
+    else
+    {
+        node *root = new node(data);
+        cout << "Root data is : " << root->data << endl;
+        cout << root->data << " left -> " << endl;
+        root->left = tree();
+        cout << root->data << " right -> " << endl;
+        root->right = tree();
+        return root;
+    }
+}
+void printbottomView(node *root)
+{
+    map<int, int> hdToNodemap;
+    queue<pair<node *, int>> q;
+    q.push(make_pair(root, 0));
+
+    while (!q.empty())
+    {
+        pair<node *, int> temp = q.front();
+        q.pop();
+
+        node *frontNode = temp.first;
+        int hd = temp.second;
+
+        hdToNodemap[hd] = frontNode->data;
+
+        // child ko dekhna h
+        if (frontNode->left != NULL)
+        {
+            q.push(make_pair(frontNode->left, hd - 1));
+        }
+        if (frontNode->right != NULL)
+        {
+            q.push(make_pair(frontNode->right, hd + 1));
+        }
+    }
+    cout << "printing top view: " << endl;
+    for (auto i : hdToNodemap)
+    {
+        cout << i.second << " ";
+    }
+}
+int main()
+{
+    node *root = tree();
+    printbottomView(root);
+
+    return 0;
+}
